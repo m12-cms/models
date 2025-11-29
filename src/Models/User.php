d0d79f722950c0f6ba9327e82693e273900cfcbe
Vladas Lapinskas <vlad.lapinskas@gmail.com>
2025-11-29 11:08:46 +0000

    User model

---
+++ src/Models/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace M12\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use Notifiable;
+
+    /**
+     * Mass assignable attributes.
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Hidden attributes.
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Casts.
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
