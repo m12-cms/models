aebed79f62d8abe58ef35066489473d47974b5bd
Vladas Lapinskas <vlad.lapinskas@gmail.com>
2025-12-01 16:29:11 +0000

    Fix contract

--- src/Models/User.php
+++ src/Models/User.php
@@ -11,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements FilamentUserContract, HasAvatar
 {
-    use FilamentUser;
+    use FilamentUserContract;
     use HasApiTokens;
     use Notifiable;
 
