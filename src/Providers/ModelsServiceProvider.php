d7636777a3d751f0e610163724e11e8b09e67857
Vladas Lapinskas <vlad.lapinskas@gmail.com>
2025-12-06 00:22:46 +0000

    Pint fixes

--- src/Providers/ModelsServiceProvider.php
+++ src/Providers/ModelsServiceProvider.php
@@ -8,6 +8,6 @@ class ModelsServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
     }
 }
